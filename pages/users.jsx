6505a307bb21881dd084430ed2d59e1bace46e5d
88ta <macyahashun@gmail.com>
2018-03-28 16:22:43 +0900

    Added user list

--- pages/users.jsx
+++ pages/users.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import Link from 'next/link'
 import App from '../components/App'
 
 const Title = styled.h1`
@@ -8,8 +9,27 @@ const Title = styled.h1`
   margin-bottom: 20px;
 `
 
+const UserList = styled.ul`
+  li {
+    margin-bottom: 20px;
+  }
+  a {
+    color: black;
+    border-radius: 4px;
+    text-decoration: none;
+    display: block;
+    padding: 40px;
+      box-shadow: 0 1px 2px 0 rgba(40, 60, 80, 0.24), 0 -1px 2px 0 rgba(40, 60, 80, 0.12);
+
+  }
+`
 export default () => (
   <App title="Next.js - Users">
     <Title>Users</Title>
+    <UserList>
+      {Array.from(new Array(20)).map((val, key) => (
+        <li key={key}><Link href="/user/1">User</Link></li>
+      ))}
+    </UserList>
   </App>
 )
