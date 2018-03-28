cf93a1ed96c6970829ec4b6369e24d83fdc02715
88ta <macyahashun@gmail.com>
2018-03-28 16:57:36 +0900

    Fixed top page style

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,6 +7,10 @@ const Title = styled.h1`
   font-weight: bold;
   margin-bottom: 20px;
 `
+const Description = styled.div`
+  line-height: 40px;
+  height: 200px;
+`
 
 export default () => (
   <App
@@ -14,5 +18,9 @@ export default () => (
     description="This is top page of NextJS sample"
   >
     <Title>Home</Title>
+    <Description>
+      Next.js is a minimalistic framework for server-rendered React applications.
+      Visit <a href="https://github.com/zeit/next.js/">Github</a> to get started with Next.js.
+    </Description>
   </App>
 )
